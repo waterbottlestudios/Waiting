df2966cc5e3186bf04e75925140cb0c06d8da9ce
Water Bottle Studios <waterbottlestudios@gmail.com>
2011-07-19 09:31:53 -0500

    Added the files for the app to function

---
+++ Waiting/timer.c
@@ -0,0 +1,9 @@
+//
+//  timer.c
+//  Waiting
+//
+//  Created by Dylan on 11-07-19.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "timer.h"
---
+++ Waiting/timer.h
@@ -0,0 +1,7 @@
+//
+//  timer.h
+//  Waiting
+//
+//  Created by Dylan on 11-07-19.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
