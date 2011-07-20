2d72cc4994d8cccf4a8a7613b510485f9c580f74
Water Bottle Studios <waterbottlestudios@gmail.com>
2011-07-20 15:02:12 -0500

    Many bug fixes, added black screen bug, added navigation controller on top of tab bar controller, redone UI, fixed about button, work on timer, and general code improvement.

---
+++ NavClass.h
@@ -0,0 +1,18 @@
+//
+//  NavClass.h
+//  Waiting
+//
+//  Created by Dylan on 11-07-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NavClass : UIViewController {
+    
+}
+
+//-(IBAction)about;
+
+@end
---
+++ NavClass.m
@@ -0,0 +1,70 @@
+//
+//  NavClass.m
+//  Waiting
+//
+//  Created by Dylan on 11-07-20.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NavClass.h"
+
+
+@implementation NavClass
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+/*-(IBAction)about {
+    UIAlertView *about = [[UIAlertView alloc]
+                          initWithTitle: @"About"
+                          message: @"This"
+                          delegate:nil
+                          cancelButtonTitle: @"Close"
+                          otherButtonTitles:nil, nil];
+    [about show];
+    [about release];
+    
+}*/
+
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
